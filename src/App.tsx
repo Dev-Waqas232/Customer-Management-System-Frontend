c3b3a3ebeaaa62860e3d25db5367fc1834bcbf12
Dev-Waqas232 <devwaqas232@gmail.com>
2025-12-20 19:07:19 +0500

    feat: added new project

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="text-white h-screen flex justify-center items-center bg-linear-to-br from-teal-600 to-teal-900 text-3xl font-semibold">
+      Hello World
+    </div>
+  );
+}
